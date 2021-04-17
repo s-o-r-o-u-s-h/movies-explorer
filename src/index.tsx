868c2704d302a0f5b8219ac1c47a3b023c8750d6
Soroush <soroush.gholamzadeh@devolon.fi>
2021-04-17 19:37:05 +0400

    - Added styled-components to the project - Restructured the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import App from './components/App';
+import GlobalStyle from './components/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
